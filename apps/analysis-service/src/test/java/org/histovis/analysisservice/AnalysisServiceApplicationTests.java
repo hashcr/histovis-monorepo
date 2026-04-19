13d5b0976cc0b70be571b66bbf812886e496813c
Ashuin Sharma Chamorro <81604231+hashcr@users.noreply.github.com>
2026-04-19 11:51:59 -0600

    Fix analysis tests

--- apps/analysis-service/src/test/java/org/histovis/analysisservice/AnalysisServiceApplicationTests.java
+++ apps/analysis-service/src/test/java/org/histovis/analysisservice/AnalysisServiceApplicationTests.java
@@ -1,12 +1,25 @@
 package org.histovis.analysisservice;
 
+import org.histovis.analysisservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
+@ActiveProfiles("test")
 class AnalysisServiceApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 }
