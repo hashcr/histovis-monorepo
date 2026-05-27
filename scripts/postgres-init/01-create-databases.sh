a3bdfc62d35a6744278ca13095e4e2dd412c5ad8
Ashuin Sharma Chamorro <81604231+hashcr@users.noreply.github.com>
2026-05-26 23:33:04 -0600

    [plugin] Fix db schemas

--- scripts/postgres-init/01-create-databases.sh
+++ scripts/postgres-init/01-create-databases.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Creates additional databases beyond the default POSTGRES_DB.
-# The default DB (POSTGRES_DB) is already created by the postgres image.
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    SELECT 'CREATE DATABASE "${ANALYSIS_POSTGRES_DB}"'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${ANALYSIS_POSTGRES_DB}')
-    \gexec
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA IF NOT EXISTS user_service;
+    CREATE SCHEMA IF NOT EXISTS images_service;
+    CREATE SCHEMA IF NOT EXISTS analysis_service;
 EOSQL
