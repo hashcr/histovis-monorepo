05105058d6158017f5b3adc9b4a60088369b2672
Ashuin Sharma <ashuin.sharma@gmail.com>
2026-06-01 14:19:36 -0600

    [security] Update CORS

--- apps/images-service/src/main/java/org/histovis/imagesservice/config/SecurityConfig.java
+++ apps/images-service/src/main/java/org/histovis/imagesservice/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8100", "http://localhost", "https://histovis-app.ashuin-sharma.workers.dev"));
+        config.setAllowedOrigins(List.of("http://localhost:8100", "http://localhost", "https://histovis-app.ashuin-sharma.workers.dev", "https://histovis.online", "https://www.histovis.online"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
